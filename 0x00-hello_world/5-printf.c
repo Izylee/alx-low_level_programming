6e660c99c9135afed437373a8e27f3090a634135
izylee <izylee100@gmail.com>
2023-02-16 18:31:21 -0800

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The main carries the code
+ *
+ * Return:always brings back 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+
+}
