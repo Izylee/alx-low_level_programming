b45c400145bb5e51cca4520e163847411bc8ba3b
izylee <izylee100@gmail.com>
2023-03-07 05:42:26 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - entry point value
- * @s: pointed destination
- * @b: constant bytes
- * @n: bytes
- * Return - Always 0 (success)
+ *_memset - fill a block of memory with empty value
+ *Description: 'the program's descriptio
+ *@s: starting address of memory to be filled
+ *@b: The derived value
+ *@n: number of the bytes to be changed
+ *Return: changed away value for n bytes
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0;
 
-	for (i = 0; i > n; i++)
+
+	for (; i > 0; i++)
+	{
 		s[i] = b;
+		n--;
+	}
 	return (s);
 }
