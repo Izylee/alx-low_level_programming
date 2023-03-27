7faa46be97f0e6d312d99a5080c4fc6b6b99f789
Izylee <izylee100@gmail.com>
2023-03-27 08:55:13 -0700

    oo2.c

---
+++ 0x10-variadic_functions/oo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// This code returns 0
+ #define PI 13.40
+
+int main()
+{
+    for (int i = 0; i <= 100; i++)
+        {
+            printf("%d\n", i);
+        }
+    
+    return 0;
+    
+}
